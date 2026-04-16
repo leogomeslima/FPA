b2f63290e703986f7a68aee110e41bafdfe9a065
leogomeslima <leonardo.glima2017@gmail.com>
2026-04-15 22:51:22 -0300

    feat: cria a stored procedure sp_RelatorioDRE para geração de relatórios financeiros

---
+++ database/10_sp_relatorio_dre.sql
@@ -0,0 +1,33 @@
+﻿CREATE OR ALTER PROCEDURE sp_RelatorioDRE
+    @Ano INT,
+    @Mes INT = NULL,
+    @Cenario NVARCHAR(50) = NULL
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT
+        [Ano],
+        [Mes],
+        NomeMes,
+        Cenario,
+        GrupoDRE,
+        SUM(Valor) AS ValorTotal
+    FROM vw_AnaliseFinanceira
+    WHERE [Ano] = @Ano
+      AND (@Mes IS NULL OR [Mes] = @Mes)
+      AND (@Cenario IS NULL OR Cenario = @Cenario)
+      AND TipoConta IN ('Receita', 'Despesa', 'CAPEX')
+    GROUP BY
+        [Ano],
+        [Mes],
+        NomeMes,
+        Cenario,
+        GrupoDRE
+    ORDER BY
+        [Ano],
+        [Mes],
+        Cenario,
+        GrupoDRE;
+END;
+GO
