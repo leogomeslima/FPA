adae3a88a7ffdb280f599291afaaed570c7c2b09
leogomeslima <leonardo.glima2017@gmail.com>
2026-04-15 23:06:44 -0300

    feat: cria a stored procedure sp_PrevisaoRolling para previsão de valores financeiros

---
+++ database/13_sp_previsao_rolling.sql
@@ -0,0 +1,47 @@
+﻿CREATE OR ALTER PROCEDURE sp_PrevisaoRolling
+    @Ano INT,
+    @UltimoMesFechado INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @IdVersaoAtiva INT;
+
+    SELECT TOP 1
+        @IdVersaoAtiva = Id
+    FROM VersoesPrevisao
+    WHERE EstaAtivo = 1
+    ORDER BY CriadoEm DESC;
+
+    SELECT
+        af.[Ano],
+        af.[Mes],
+        af.NomeMes,
+        af.CodigoConta,
+        af.NomeConta,
+        af.GrupoDRE,
+        SUM(af.Valor) AS ValorRolling
+    FROM vw_AnaliseFinanceira af
+    WHERE af.[Ano] = @Ano
+      AND
+      (
+          (af.[Mes] <= @UltimoMesFechado AND af.Cenario = 'Realizado')
+          OR
+          (
+              af.[Mes] > @UltimoMesFechado
+              AND af.Cenario = 'Previsao'
+              AND af.VersaoPrevisaoId = @IdVersaoAtiva
+          )
+      )
+    GROUP BY
+        af.[Ano],
+        af.[Mes],
+        af.NomeMes,
+        af.CodigoConta,
+        af.NomeConta,
+        af.GrupoDRE
+    ORDER BY
+        af.[Mes],
+        af.CodigoConta;
+END;
+GO
