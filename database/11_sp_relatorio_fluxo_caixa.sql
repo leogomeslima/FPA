a17e9fbb4721b4235da2ef0d4c8bb52879b8b850
leogomeslima <leonardo.glima2017@gmail.com>
2026-04-15 22:51:33 -0300

    feat: cria a stored procedure sp_RelatorioFluxoCaixa para geração de relatórios de fluxo de caixa

---
+++ database/11_sp_relatorio_fluxo_caixa.sql
@@ -0,0 +1,32 @@
+﻿CREATE OR ALTER PROCEDURE sp_RelatorioFluxoCaixa
+    @Ano INT,
+    @Mes INT = NULL,
+    @Cenario NVARCHAR(50) = NULL
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT
+        [Ano],
+        [Mes],
+        NomeMes,
+        Cenario,
+        GrupoFluxoCaixa,
+        SUM(Valor) AS ValorFluxoCaixa
+    FROM vw_AnaliseFinanceira
+    WHERE [Ano] = @Ano
+      AND (@Mes IS NULL OR [Mes] = @Mes)
+      AND (@Cenario IS NULL OR Cenario = @Cenario)
+    GROUP BY
+        [Ano],
+        [Mes],
+        NomeMes,
+        Cenario,
+        GrupoFluxoCaixa
+    ORDER BY
+        [Ano],
+        [Mes],
+        Cenario,
+        GrupoFluxoCaixa;
+END;
+GO
