1b690c86d3cd6a9e7d6b86f7b289be0e1072813a
leogomeslima <leonardo.glima2017@gmail.com>
2026-04-15 22:51:44 -0300

    feat: cria a stored procedure sp_ComparativoOrcadoRealizado para comparação de valores orçados e realizados

---
+++ database/12_sp_comparativo_orcado_realizado.sql
@@ -0,0 +1,49 @@
+﻿CREATE OR ALTER PROCEDURE sp_ComparativoOrcadoRealizado
+    @Ano INT,
+    @Mes INT = NULL
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @IdVersaoAtiva INT;
+
+    SELECT TOP 1
+        @IdVersaoAtiva = Id
+    FROM VersoesPrevisao
+    WHERE EstaAtivo = 1
+    ORDER BY CriadoEm DESC;
+
+    SELECT
+        af.[Ano],
+        af.[Mes],
+        af.NomeMes,
+        af.CodigoConta,
+        af.NomeConta,
+        af.GrupoDRE,
+        SUM(CASE WHEN af.Cenario = 'Realizado' THEN af.Valor ELSE 0 END) AS ValorRealizado,
+        SUM(CASE WHEN af.Cenario = 'Orcado' THEN af.Valor ELSE 0 END) AS ValorOrcado,
+        SUM(CASE WHEN af.Cenario = 'Previsao'
+                  AND af.VersaoPrevisaoId = @IdVersaoAtiva
+                 THEN af.Valor ELSE 0 END) AS ValorPrevisao,
+        SUM(CASE WHEN af.Cenario = 'Realizado' THEN af.Valor ELSE 0 END)
+            - SUM(CASE WHEN af.Cenario = 'Orcado' THEN af.Valor ELSE 0 END) AS VariancaOrcado,
+        SUM(CASE WHEN af.Cenario = 'Realizado' THEN af.Valor ELSE 0 END)
+            - SUM(CASE WHEN af.Cenario = 'Previsao'
+                        AND af.VersaoPrevisaoId = @IdVersaoAtiva
+                       THEN af.Valor ELSE 0 END) AS VariancaPrevisao
+    FROM vw_AnaliseFinanceira af
+    WHERE af.[Ano] = @Ano
+      AND (@Mes IS NULL OR af.[Mes] = @Mes)
+    GROUP BY
+        af.[Ano],
+        af.[Mes],
+        af.NomeMes,
+        af.CodigoConta,
+        af.NomeConta,
+        af.GrupoDRE
+    ORDER BY
+        af.[Ano],
+        af.[Mes],
+        af.CodigoConta;
+END;
+GO
